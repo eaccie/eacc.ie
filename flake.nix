d42a7deb1f2eb5022b223f9ff0f95e8df1e92563
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2024-02-17 21:13:57 +0100

    Prettier inits (#7)
    * Formatting changes
    
    * Extend fmt beyond .astro files

--- flake.nix
+++ flake.nix
@@ -13,9 +13,7 @@
       systems =
         [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }: {
-        devshells.default = {
-          packages = with pkgs; [ nodejs_21 nodePackages_latest.prettier ];
-        };
+        devshells.default = { packages = with pkgs; [ nodejs_21 ]; };
       };
       flake = { };
     };
